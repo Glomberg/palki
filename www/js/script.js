9cc04cdb6ab50bd85bd195703b14fcbf623e3669
Viktor <bazz@bk.ru>
2016-06-29 23:22:53 +0300

    123

--- www/js/script.js
+++ www/js/script.js
@@ -1,4 +1,4 @@
-﻿$(document).ready(function(){
+$(document).ready(function(){
 	$("a.scroll").on("click", function(e){
 		e.preventDefault();
 		href = $(this).attr("href");
@@ -6,4 +6,16 @@
 			$("body,html").stop().animate({ scrollTop: $(href).offset().top - 110}, 1000);
 		}
 	});
+	//ползунок
+	$("#slider").slider({
+		value:175,
+		min: 140,
+		max: 210,
+		step: 1,
+		animate: "slow",
+		slide: function( event, ui ) {
+			$( "#amount" ).val( "$" + ui.value );
+		}
+	});
+	$( "#amount" ).val( "$" + $( "#slider" ).slider( "value" ) );
 });
